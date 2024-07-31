f6795536f897d1c0efdafd5e31efb7159163e913
Lucas Rodrigues Santos <lucasrs.rodrigues3@outlook.com>
2024-07-31 15:10:16 -0300

    Initial Commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Lucas Rodrigues on 31/07/24.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
