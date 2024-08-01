2cd85cb477399aa90c20ed40520a9bc3547bab45
Lucas Rodrigues Santos <lucasrs.rodrigues3@outlook.com>
2024-08-01 11:01:30 -0300

    finished lesson

--- Hike/Component/CustomBackgroundView.swift
+++ Hike/Component/CustomBackgroundView.swift
@@ -9,10 +9,29 @@ import SwiftUI
 
 struct CustomBackgroundView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color.customGreenDark
+                .cornerRadius(40)
+                .offset(y:12)
+            
+            Color.customGreenLight
+                .cornerRadius(40)
+                .offset(y: 3)
+                .opacity(0.85)
+            
+            LinearGradient(
+            colors: [
+                Color.customGreenLight,
+                Color.customGreenMedium
+            ],
+            startPoint: .top,
+            endPoint: .bottom
+            ).cornerRadius(40)
+        }
     }
 }
 
 #Preview {
     CustomBackgroundView()
+        .padding()
 }
--- Hike/Utility/GradientButtonStyle.swift
+++ Hike/Utility/GradientButtonStyle.swift
@@ -6,3 +6,38 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct GradientButton: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration
+            .label
+            .padding(.vertical)
+            .padding(.horizontal, 30)
+            .background(
+                // Conditional Statement with Nil Coalescing
+                // Condition ? A : B
+                configuration.isPressed ?
+                LinearGradient(
+                    colors: [
+                        .customGrayMedium,
+                            .customGrayLight
+                    ],
+                    startPoint: .top,
+                    endPoint: .bottom
+                )
+                // A: When user pressed the Button
+                // B: When the Button is not pressed
+                :
+                LinearGradient(
+                    colors: [
+                        .customGrayLight,
+                            .customGrayMedium
+                    ],
+                    startPoint: .top,
+                    endPoint: .bottom
+                )
+            )
+            .cornerRadius(40)
+    }
+}
